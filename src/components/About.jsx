0c0a204e0f36db8d3917beddd4dc080ad9ec184f
Zahid Naeem <alabtaaldevelopers@gmail.com>
2024-09-10 19:45:27 +0500

    "About" contents updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 const About = ()=> {
   return (
-      <div className="container">
+      <div className="container" style={{textAlign: 'center', marginBottom: '100px'}}>
         <h1>Al-Abtaal</h1>
         <p>Please call 0314-3582009 for contact or query</p>
       </div>
